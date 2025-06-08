d8e1bb34847186c901cff9efc90375c93a3902a4
Luiz Fellipe <78910212+luiz5421@users.noreply.github.com>
2025-06-08 18:53:04 -0300

    Add files via upload

---
+++ prjestoque/src/main/java/com/PrjEstoque/prjestoque/PrjestoqueApplication.java
@@ -0,0 +1,13 @@
+package com.PrjEstoque.prjestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjestoqueApplication.class, args);
+	}
+
+}
---
+++ prjestoque/target/classes/schema-postgressql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS produto (
+    id serial PRIMARY KEY,
+    nome varchar(100),
+    valor float,
+    categoria varchar(100),
+    quantidade int
+);
