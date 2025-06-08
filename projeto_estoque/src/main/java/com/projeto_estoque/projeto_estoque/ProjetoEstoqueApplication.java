e3db9db29b9d25c54c814e68c50e4de57820368b
Luiz Fellipe <78910212+luiz5421@users.noreply.github.com>
2025-06-08 12:14:27 -0300

    Add files via upload

---
+++ projeto_estoque/src/main/java/com/projeto_estoque/projeto_estoque/ProjetoEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_estoque.projeto_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEstoqueApplication.class, args);
+	}
+
+}
